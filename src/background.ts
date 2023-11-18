4205e6728c1b4f72c87a78aa74bc7234ecbca319
Sailwayfun <liaoleon000513@gmail.com>
2023-11-18 09:36:43 +0800

    Fix: change service worker to .ts

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
   if (request.action == "getTabs") {
     chrome.tabs.query({ currentWindow: true }, function (tabs) {
       sendResponse({ tabs });
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,15 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         newtab: resolve(__dirname, "newTab.html"),
+        background: resolve(__dirname, "./src/background.ts"),
+      },
+      output: {
+        format: "esm",
+        dir: "dist",
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === "background") return "background.js";
+          return "[name]-[hash].js";
+        },
       },
     },
   },

8924a43a57b1f3a93fe4d35744ab6c7d7ce2377f
Sailwayfun <liaoleon000513@gmail.com>
2023-11-19 09:26:38 +0800

    Create Spaces section

---
+++ src/components/NewTab/Spaces.tsx
@@ -0,0 +1,20 @@
+const Spaces = () => {
+  return (
+    <div className="flex w-80 bg-red-800">
+      <ul className="my-44 flex w-full flex-col">
+        <li className="border px-2 py-4 text-white">Space</li>
+        <li className="border px-2 py-4 text-white">Space</li>
+        <li className="border px-2 py-4 text-white">Space</li>
+        <li className="border px-2 py-4 text-white">Space</li>
+        <li className="border px-2 py-4 text-white">Space</li>
+        <li className="border px-2 py-4 text-white">Space</li>
+        <li className="border px-2 py-4 text-white">Space</li>
+        <li className="border px-2 py-4 text-white">Space</li>
+        <li className="border px-2 py-4 text-white">Space</li>
+        <li className="border px-2 py-4 text-white">Space</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Spaces;
--- src/components/NewTab/index.tsx
+++ src/components/NewTab/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { FieldValue } from "firebase/firestore";
+import Spaces from "./Spaces";
 interface Tab extends chrome.tabs.Tab {
   lastAccessed: FieldValue;
 }
@@ -21,27 +22,30 @@ const NewTab = () => {
     chrome.tabs.create({ url: newTabUrl });
   }
   return (
-    <div className="mx-auto w-full px-80 py-8">
-      <h1 className="mb-4 text-3xl">Your Tabs</h1>
-      <ul className="flex flex-col gap-3">
-        {tabs.length > 0 &&
-          tabs.map((tab, index) => {
-            return (
-              <li
-                key={index}
-                className="flex items-center gap-3 rounded-lg border px-4 py-2 text-lg"
-              >
-                <img src={tab.favIconUrl} className="h-4 w-4 bg-gray-500" />
-                <a
-                  onClick={(e) => openLink(e, tab)}
-                  className="cursor-pointer hover:text-gray-500 hover:underline"
+    <div className="flex w-full px-4 py-8">
+      <Spaces />
+      <div className="flex flex-col">
+        <h1 className="mb-4 text-3xl">Your Tabs</h1>
+        <ul className="flex flex-col gap-3">
+          {tabs.length > 0 &&
+            tabs.map((tab, index) => {
+              return (
+                <li
+                  key={index}
+                  className="flex items-center gap-3 rounded-lg border px-4 py-2 text-lg"
                 >
-                  {tab.title}
-                </a>
-              </li>
-            );
-          })}
-      </ul>
+                  <img src={tab.favIconUrl} className="h-4 w-4 bg-gray-500" />
+                  <a
+                    onClick={(e) => openLink(e, tab)}
+                    className="cursor-pointer hover:text-gray-500 hover:underline"
+                  >
+                    {tab.title}
+                  </a>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
     </div>
   );
 };

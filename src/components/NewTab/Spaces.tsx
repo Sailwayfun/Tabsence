4fbe67107b0702c0597da82b43f587ab9b232f7d
Sailwayfun <liaoleon000513@gmail.com>
2023-11-19 09:37:52 +0800

    Create static space names

--- src/components/NewTab/Spaces.tsx
+++ src/components/NewTab/Spaces.tsx
@@ -2,16 +2,18 @@ const Spaces = () => {
   return (
     <div className="flex w-80 bg-red-800">
       <ul className="my-44 flex w-full flex-col">
-        <li className="border px-2 py-4 text-white">Space</li>
-        <li className="border px-2 py-4 text-white">Space</li>
-        <li className="border px-2 py-4 text-white">Space</li>
-        <li className="border px-2 py-4 text-white">Space</li>
-        <li className="border px-2 py-4 text-white">Space</li>
-        <li className="border px-2 py-4 text-white">Space</li>
-        <li className="border px-2 py-4 text-white">Space</li>
-        <li className="border px-2 py-4 text-white">Space</li>
-        <li className="border px-2 py-4 text-white">Space</li>
-        <li className="border px-2 py-4 text-white">Space</li>
+        <li className="border px-2 py-4 text-white">
+          <a href="#">Unsaved</a>
+        </li>
+        <li className="border px-2 py-4 text-white">AppWorks School</li>
+        <li className="border px-2 py-4 text-white">Family</li>
+        <li className="border px-2 py-4 text-white">Game</li>
+        <li className="border px-2 py-4 text-white">Anime</li>
+        <li className="border px-2 py-4 text-white">Netflix</li>
+        <li className="border px-2 py-4 text-white">Trip</li>
+        <li className="border px-2 py-4 text-white">Career</li>
+        <li className="border px-2 py-4 text-white">Cooking</li>
+        <li className="border px-2 py-4 text-white">Sports</li>
       </ul>
     </div>
   );

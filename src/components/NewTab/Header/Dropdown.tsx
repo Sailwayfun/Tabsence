6353832465f56596056b8b3d0a4de4cc75ab3549
Sailwayfun <liaoleon000513@gmail.com>
2023-12-03 14:54:51 +0800

    Change drop down icon to hamburger

--- src/components/NewTab/Header/Dropdown.tsx
+++ src/components/NewTab/Header/Dropdown.tsx
@@ -21,7 +21,7 @@ const Dropdown = ({ onCopySpaceLink }: DropdownProps) => {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
-            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            d="M4 6h16M4 12h16M4 18h16"
           ></path>
         </svg>
       </div>

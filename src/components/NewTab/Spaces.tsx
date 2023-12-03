f3faf3c7b4dadb311779c062779aa13cf65e74eb
Sailwayfun <liaoleon000513@gmail.com>
2023-12-03 22:46:21 +0800

    Remove comments

--- src/components/NewTab/Spaces.tsx
+++ src/components/NewTab/Spaces.tsx
@@ -74,26 +74,6 @@ const Spaces = forwardRef(
         </div>
         <h2 className="self-end pr-4 pt-12 text-xl text-white">Spaces</h2>
         <AddSpaceBtn onAddSpace={onOpenAddSpacePopup} />
-        {/* <div
-          className={`absolute left-20 top-40 z-10 flex h-36 w-60 flex-col gap-3 border bg-white p-4 shadow ${
-            isAddSpacePopupOpen ? "block" : "hidden"
-          }`}
-        >
-          <button
-            onClick={onCloseAddSpacePopup}
-            className="absolute right-0 top-0 rounded-sm bg-red-500 p-2 text-white"
-          >
-            X
-          </button>
-          <label className="text-xl">New Space:</label>
-          <input className="h-4 w-full rounded-lg border p-4" ref={ref} />
-          <button
-            onClick={onAddNewSpace}
-            className="flex h-8 w-full items-center justify-center rounded-md border px-4 py-2 text-gray-500 hover:bg-gray-500 hover:text-white"
-          >
-            Add a new space
-          </button>
-        </div> */}
         <AddSpace ref={ref} onAddNewSpace={handleAddNewSpace} />
         <ul className="flex w-full flex-col">
           {spaces.map(({ id, title }) => {

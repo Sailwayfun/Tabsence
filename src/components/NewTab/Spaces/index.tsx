22614fc07adf37fde6246c28b13c004463894ecf
Sailwayfun <liaoleon000513@gmail.com>
2023-12-07 20:06:52 +0800

    style: add divider between & before spaces

--- src/components/NewTab/Spaces/index.tsx
+++ src/components/NewTab/Spaces/index.tsx
@@ -79,6 +79,7 @@ const Spaces = forwardRef(
           </div>
           <AddSpaceBtn onAddSpace={onOpenAddSpacePopup} />
           <AddSpace ref={ref} onAddNewSpace={handleAddNewSpace} />
+          <span className="mx-auto h-[1px] w-full bg-white opacity-70" />
           <ul className="flex flex-col">
             {spaces.map(({ id, title }) => {
               const linkClasses: string = `${

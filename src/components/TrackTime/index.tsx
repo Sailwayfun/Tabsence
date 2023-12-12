9ede9581c7416b7fbebaed686d1749ec3f62929e
Sailwayfun <liaoleon000513@gmail.com>
2023-12-12 14:59:39 +0800

    feat: create a 404 page for no-data on /webtime
    feat: create a 404 page for no-data on /webtime

--- src/components/TrackTime/index.tsx
+++ src/components/TrackTime/index.tsx
@@ -1,3 +1,4 @@
+import noData from "./no-data.png";
 import {
   collection,
   query,
@@ -84,48 +85,62 @@ const TrackTime = () => {
   return (
     <>
       <Header />
-      <div className="min-h-screen max-w-full rounded-lg border bg-slate-100 p-8 shadow-md">
-        <div className="mb-3 grid grid-cols-4 text-lg">
-          {labelFields.map((label, index) => (
-            <label key={index} className="mx-auto my-0">
-              {label}
-            </label>
-          ))}
-        </div>
-        <div>
-          <ul className="flex flex-col gap-3">
-            {urlDurations.map((website) => (
-              <li
-                key={website.id}
-                className="grid w-full grid-cols-4 rounded-md border border-gray-300 p-3 shadow transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-slate-300 hover:shadow-lg xl:text-2xl"
-              >
-                <div className="flex gap-2 text-xl">
-                  <img
-                    className="h-8 w-8"
-                    src={website.faviconUrl}
-                    alt={website.id}
-                  />
-                  <span>{website.id}</span>
-                </div>
-                <div className="mx-auto my-0 text-lg">
-                  {website.durationBySecond}
-                </div>
-                <div className="mx-auto my-0 text-xl">
-                  {website.visitCounts}
-                </div>
-                <div className="mx-auto my-0 text-xl">
-                  {getDurationPercentge(website.durationBySecond)}
-                </div>
-              </li>
+      {urlDurations.length > 0 && (
+        <div className="min-h-screen max-w-full rounded-lg border bg-slate-100 p-8 shadow-md">
+          <div className="mb-3 grid grid-cols-4 text-lg">
+            {labelFields.map((label, index) => (
+              <label key={index} className="mx-auto my-0">
+                {label}
+              </label>
             ))}
-          </ul>
-          <div className="mx-auto my-3 border-t-2 border-gray-200 pt-3 text-xl">
-            <span className="pl-80 pr-2 tracking-wide">Total Duration:</span>
-            <span>{getTotalDuration()} s</span>
           </div>
+          <div>
+            <ul className="flex flex-col gap-3">
+              {urlDurations.map((website) => (
+                <li
+                  key={website.id}
+                  className="grid w-full grid-cols-4 rounded-md border border-gray-300 p-3 shadow transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-slate-300 hover:shadow-lg xl:text-2xl"
+                >
+                  <div className="flex gap-2 text-xl">
+                    <img
+                      className="h-8 w-8"
+                      src={website.faviconUrl}
+                      alt={website.id}
+                    />
+                    <span>{website.id}</span>
+                  </div>
+                  <div className="mx-auto my-0 text-lg">
+                    {website.durationBySecond}
+                  </div>
+                  <div className="mx-auto my-0 text-xl">
+                    {website.visitCounts}
+                  </div>
+                  <div className="mx-auto my-0 text-xl">
+                    {getDurationPercentge(website.durationBySecond)}
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className="mx-auto my-3 border-t-2 border-gray-200 pt-3 text-xl">
+              <span className="pl-80 pr-2 tracking-wide">Total Duration:</span>
+              <span>{getTotalDuration()} s</span>
+            </div>
+          </div>
+          <Chart durationData={urlDurations} />
         </div>
-        <Chart durationData={urlDurations} />
-      </div>
+      )}
+      {urlDurations.length === 0 && (
+        <div className="flex min-h-screen max-w-full flex-col items-center gap-4 rounded-lg border bg-slate-100 p-8 shadow-md">
+          <img src={noData} alt="no data" className="mx-auto w-full" />
+          <span className="self-end">
+            Image by{" "}
+            <a href="https://www.freepik.com/free-vector/flat-design-no-data-illustration_47718912.htm#query=not%20data&position=5&from_view=search&track=ais&uuid=7ee5c7a8-e536-4bdb-9331-39411353fc99">
+              Freepik
+            </a>
+          </span>
+          <div className="text-6xl">Sorry! There's no data.</div>
+        </div>
+      )}
     </>
   );
 };

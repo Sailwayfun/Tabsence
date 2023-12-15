32ac55429f73497240788491f6950d09d10f2430
Sailwayfun <liaoleon000513@gmail.com>
2023-12-15 15:15:42 +0800

    feat: add a button to return webtime to today

--- src/components/TrackTime/Header.tsx
+++ src/components/TrackTime/Header.tsx
@@ -17,6 +17,13 @@ const Header = () => {
     <div className="flex w-full items-center justify-between pb-4">
       <h1 className="text-3xl font-bold">Your Time Spent on Websites</h1>
       <div className="flex items-center justify-center gap-2">
+        {!(date && date === today) && (
+          <Link to={today} className="contents w-full">
+            <button className="mr-8 h-8 w-20 rounded-md bg-orange-700 bg-opacity-70 px-3 text-lg text-white shadow hover:bg-orange-900">
+              Today
+            </button>
+          </Link>
+        )}
         <Link to={getPrevDate()} className="contents w-full">
           <button
             className="h-8 w-8 cursor-pointer text-4xl"

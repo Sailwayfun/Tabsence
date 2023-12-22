21a71e5f5efc17b6ee31d523f94bb21b14b95345
Sailwayfun <liaoleon000513@gmail.com>
2023-12-22 19:08:31 +0800

    refactor(logo): styles with cn function

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,5 +1,6 @@
 import logo from "./logo.png";
 import { Link } from "react-router-dom";
+import { cn } from "../../utils";
 
 interface LogoProps {
   isWebtimePage: boolean;
@@ -7,22 +8,19 @@ interface LogoProps {
 
 const Logo = ({ isWebtimePage }: LogoProps) => {
   return (
-    <div className="h-16">
-      <Link
-        className="btn btn-ghost h-full justify-start rounded-sm pl-0 text-xl"
-        to="/"
-      >
-        <img
-          src={logo}
-          className={
-            isWebtimePage
-              ? "max-w-[150px] object-contain invert filter"
-              : `w-1/2 object-contain`
-          }
-        />
-        <img src="/icons/tabs.png" className="block h-12 w-12" />
-      </Link>
-    </div>
+    <Link
+      className="btn btn-ghost h-16 justify-start rounded-sm pl-0 text-xl"
+      to="/"
+    >
+      <img
+        src={logo}
+        alt="logo"
+        className={cn("w-1/2 object-contain", {
+          "max-w-[200px] invert filter": isWebtimePage,
+        })}
+      />
+      <img src="/icons/tabs.png" className="block h-12 w-12" />
+    </Link>
   );
 };
 

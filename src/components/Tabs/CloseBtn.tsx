fce46dc3f2e57aef5e483ce813585beecff30aac
Sailwayfun <liaoleon000513@gmail.com>
2023-12-23 00:34:31 +0800

    refactor(style): use Tooltip in close tab btn

--- src/components/Tabs/CloseBtn.tsx
+++ src/components/Tabs/CloseBtn.tsx
@@ -1,28 +1,25 @@
 interface CloseBtnProps {
   onCloseTab: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   id: string | undefined;
-  order: string;
 }
-const CloseBtn = ({ id, onCloseTab, order }: CloseBtnProps) => {
+const CloseBtn = ({ id, onCloseTab }: CloseBtnProps) => {
   return (
-    <div className={`tooltip ${order}`} data-tip="Close tab">
-      <button data-id={id} onClick={onCloseTab}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="block h-6 w-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
-    </div>
+    <button data-id={id} onClick={onCloseTab}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="block h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </button>
   );
 };
 
